5e7ad5f00759870dc28f23261d40bac7404645f8
Gulshankartikk <210281116+Gulshankartikk@users.noreply.github.com>
2025-11-15 11:22:06 +0530

    Add implementations for pointer and call by reference examples in C++

---
+++ 23lecture/05_call_by_refrence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void fun(int &c, int&d)
+{
+    c =c*2;
+    d =d*2;
+}
+
+int main()
+{
+    int a=5,b=8;
+    fun(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
